ef60136bc7d5a796c84e10ae771a365ece45b38a
tanya8090 <61150410+tanya8090@users.noreply.github.com>
2020-02-18 18:39:46 +0530

    Add files via upload

---
+++ mul.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+int a,b,mul;
+print("enter the vale of two no");
+scanf("%d%d",&a,&b);
+mul=a*b;
+printf("multiplication =%d",mul);
+return 0;
+
+}
